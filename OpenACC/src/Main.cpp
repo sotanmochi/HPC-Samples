630eeb9e60de8c471cc6dda75235f6a96ed89fa6
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2022-08-20 01:10:07 +0900

    Update OpenACC code samples

--- OpenACC/src/Main.cpp
+++ OpenACC/src/Main.cpp
@@ -8,10 +8,10 @@ int main()
     std::cout << "====================" << std::endl;
     std::cout << std::endl;
 
-    HpcSamples::Accelerator accelerator(1024);
+    HpcSamples::Accelerator accelerator(1024 + 512);
     std::cout << std::endl;
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
         std::cout << "====================" << std::endl;
         std::cout << "Count: " << i << std::endl;
